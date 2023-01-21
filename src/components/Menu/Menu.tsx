c3b168bbe2db62e2b5d11b2014246bc30d83dfb4
Gennadiy <glisovuy@ukr.net>
2023-01-21 21:44:20 +0200

    add menu component and control1 page

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,19 +4,17 @@ import './Menu.scss'
 type Props = {}
 const Menu = (props: Props) => {
     return (
-        <div>
-            <ul className='nav-menu-list'>
-                <li>
-                    <MenuItem to="/">Home</MenuItem>
-                </li>
-                <li>
-                    <MenuItem to="task1">Task 1</MenuItem>
-                </li>
-                <li>
-                    <MenuItem to="task2">Task 2</MenuItem>
-                </li>
-            </ul>
-        </div>
+        <ul className="nav-menu-list">
+            <li>
+                <MenuItem to="/">Home</MenuItem>
+            </li>
+            <li>
+                <MenuItem to="task1">Task 1</MenuItem>
+            </li>
+            <li>
+                <MenuItem to="task2">Task 2</MenuItem>
+            </li>
+        </ul>
     )
 }
 export default Menu
--- src/pages/ControlWork1/ControlWork1.tsx
+++ src/pages/ControlWork1/ControlWork1.tsx
@@ -1,7 +1,25 @@
+import { Button } from '@mui/material'
+import './ControlWork1.scss'
+
 type Props = {}
 const ControlWork1 = (props: Props) => {
-  return (
-    <div>ControlWork1</div>
-  )
+    return (
+        <div className="control1">
+            <div className="control1-content">
+                <h1>Task 1</h1>
+                <div className="btn-block">
+                    <Button className="btn" variant="outlined">
+                        Contained
+                    </Button>
+                    <Button className="btn" variant="outlined">
+                        Contained
+                    </Button>
+                    <Button className="btn" variant="outlined">
+                        Contained
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
 }
 export default ControlWork1
